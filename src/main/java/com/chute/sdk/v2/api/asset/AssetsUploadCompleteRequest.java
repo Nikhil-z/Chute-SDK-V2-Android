45f68af5b6a6915f3bd02128cc1d305e37143a62
olga <olahla@gmail.com>
2013-06-14 18:51:15 +0200

    removing bom files

--- src/main/java/com/chute/sdk/v2/api/asset/AssetsUploadCompleteRequest.java
+++ src/main/java/com/chute/sdk/v2/api/asset/AssetsUploadCompleteRequest.java
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2011, Chute Corporation. All rights reserved.
+// Copyright (c) 2011, Chute Corporation. All rights reserved.
 // 
 //  Redistribution and use in source and binary forms, with or without modification, 
 //  are permitted provided that the following conditions are met:
