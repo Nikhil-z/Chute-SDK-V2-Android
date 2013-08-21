d279d4e6679f9c361cb42b8478aef4235dc1384b
Darko Grozdanovski <darkogrozdanovski@gmail.com>
2013-08-21 13:24:59 -0700

    removed unneeded code

---
+++ src/main/java/com/chute/sdk/v2/model/interfaces/AccountMedia.java
@@ -0,0 +1,10 @@
+package com.chute.sdk.v2.model.interfaces;
+
+import com.chute.sdk.v2.model.enums.AccountMediaType;
+
+
+public interface AccountMedia {
+  
+  public AccountMediaType getViewType();
+
+}
