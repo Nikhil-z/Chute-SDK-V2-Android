be461c194df147f774a257a25a804f11ac178b8a
Darko Grozdanovski <darkogrozdanovski@gmail.com>
2015-04-30 17:58:11 +0200

    moving to gradle

---
+++ library/src/main/java/com/chute/sdk/v2/model/ProfileModel.java
@@ -0,0 +1,281 @@
+// Copyright (c) 2011, Chute Corporation. All rights reserved.
+// 
+//  Redistribution and use in source and binary forms, with or without modification, 
+//  are permitted provided that the following conditions are met:
+// 
+//     * Redistributions of source code must retain the above copyright notice, this 
+//       list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of the  Chute Corporation nor the names
+//       of its contributors may be used to endorse or promote products derived from
+//       this software without specific prior written permission.
+// 
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+//  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+//  OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+package com.chute.sdk.v2.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * The {@link ProfileModel} class holds information regarding the user profile.
+ */
+public class ProfileModel implements Parcelable {
+
+	/**
+	 * First name of the user.
+	 */
+	@JsonProperty("first_name")
+	private String firstName;
+
+	/**
+	 * Last name of the user.
+	 */
+	@JsonProperty("last_name")
+	private String lastName;
+
+	/**
+	 * User e-mail.
+	 */
+	@JsonProperty("email")
+	private String email;
+
+	/**
+	 * User phone number.
+	 */
+	@JsonProperty("phone_number")
+	private String phoneNumber;
+
+	/**
+	 * User birth year.
+	 */
+	@JsonProperty("birth_year")
+	private String birthYear;
+
+	@JsonProperty("title")
+	private String title;
+
+	@JsonProperty("company")
+	private String company;
+
+	/**
+	 * Getter and setter methods.
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(String birthYear) {
+		this.birthYear = birthYear;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	/**
+	 * Default non-args constructor.
+	 */
+	public ProfileModel() {
+	}
+
+	public ProfileModel(Parcel in) {
+		this();
+		firstName = in.readString();
+		lastName = in.readString();
+		email = in.readString();
+		phoneNumber = in.readString();
+		birthYear = in.readString();
+		title = in.readString();
+		company = in.readString();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.os.Parcelable#describeContents()
+	 */
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.os.Parcelable#writeToParcel(android.os.Parcel, int)
+	 */
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(firstName);
+		dest.writeString(lastName);
+		dest.writeString(email);
+		dest.writeString(phoneNumber);
+		dest.writeString(birthYear);
+		dest.writeString(title);
+		dest.writeString(company);
+	}
+
+	public static final Creator<ProfileModel> CREATOR = new Creator<ProfileModel>() {
+
+		@Override
+		public ProfileModel createFromParcel(Parcel in) {
+			return new ProfileModel(in);
+		}
+
+		@Override
+		public ProfileModel[] newArray(int size) {
+			return new ProfileModel[size];
+		}
+
+	};
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((birthYear == null) ? 0 : birthYear.hashCode());
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result
+				+ ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result
+				+ ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProfileModel other = (ProfileModel) obj;
+		if (birthYear == null) {
+			if (other.birthYear != null)
+				return false;
+		} else if (!birthYear.equals(other.birthYear))
+			return false;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (phoneNumber == null) {
+			if (other.phoneNumber != null)
+				return false;
+		} else if (!phoneNumber.equals(other.phoneNumber))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ProfileModel [firstName=");
+		builder.append(firstName);
+		builder.append(", lastName=");
+		builder.append(lastName);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append(", phoneNumber=");
+		builder.append(phoneNumber);
+		builder.append(", birthYear=");
+		builder.append(birthYear);
+		builder.append(", title=");
+		builder.append(title);
+		builder.append(", company=");
+		builder.append(company);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
---
+++ library/src/main/java/com/chute/sdk/v2/utils/MediaTypes.java
@@ -0,0 +1,7 @@
+package com.chute.sdk.v2.utils;
+
+import com.squareup.okhttp.MediaType;
+
+public class MediaTypes {
+  public static MediaType JSON = MediaType.parse("application/json");
+}
